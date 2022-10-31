225fcc68ca50be149c9ece4fa872b5e701825582
kulikov-dev <kulikov.dev@gmail.com>
2022-10-31 21:40:03 +0300

    Added funcion to check if user a developer

---
+++ SmallNetUtils/Utils/ShellUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace SmallNetUtils.Utils
+{
+    /// <summary>
+    /// Shell utils
+    /// </summary>
+    public static class ShellUtils
+    {
+        /// <summary>
+        /// Flag if user launcher app is a developer
+        /// </summary>
+        /// <returns> Flag if developer </returns>
+        public static bool IsDeveloper()
+        {
+            #if (DEBUG)
+            return true;
+            #else
+            return false;
+            #endif
+        }
+    }
+}
